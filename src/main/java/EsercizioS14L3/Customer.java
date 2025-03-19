b2dcd7b39a8509a0cff861a6a8486b3f71a093fd
Zephyrior <mark.temblique@gmail.com>
2025-03-19 15:45:36 +0100

    Inizio struttura esercizio

---
+++ src/main/java/EsercizioS14L3/Customer.java
@@ -0,0 +1,7 @@
+package EsercizioS14L3;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private int tier;
+}
---
+++ src/main/java/EsercizioS14L3/Order.java
@@ -0,0 +1,74 @@
+package EsercizioS14L3;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+    private Long id;
+    private String status;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private List<Product> products;
+    private String customer;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public Order(Long id, String status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, String customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public Order() {
+
+    }
+}
